c5fa6d76eeab5d163522db83f6a3c42ff1d49e83
EH225 <eric.helmold@gmail.com>
2026-01-31 16:33:53 -0500

    raise max_batches during scst eval

--- vlm_trainer.py
+++ vlm_trainer.py
@@ -953,7 +953,7 @@ class TrainerCaptioning:
             # Re-instate the model weights from the checkpoint data read in from disk
             self.vlm.load_state_dict(checkpoint_data["model"])
 
-    def compute_eval_scores(self, max_len: int = 50, max_batches: int = 10, eps: float = 0.1) -> Tuple[float]:
+    def compute_eval_scores(self, max_len: int = 50, max_batches: int = 50, eps: float = 0.1) -> Tuple[float]:
         """
         This method computes the negative log-likelihood, perplexity, and CIDEr score on the validation data
         set using the current model weights. Calling this method periodically during training is helpful for
